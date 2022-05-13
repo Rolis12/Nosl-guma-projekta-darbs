4ac7a0cdd80e4ecf9d32aaa8c781b4ad61b62e74
Rolands Cīrulis <rc2816@gymn.local>
2022-05-13 09:56:44 +0300

    Fons un masīvs

--- funkcija.js
+++ funkcija.js
@@ -5,20 +5,30 @@ input.addEventListener('change', () => {
     if(files.length == 0) return;
     const file = files[0];
     let reader = new FileReader();
+	
     reader.onload = (e) => {
         const file = e.target.result;
         const lines = file.split(/\r\n|\n/);
         textarea.value = lines.join('\n');
     };
+	var items = [files];
 	
+	var datnesSaturs; // mainīgais, kurā tiks saglabāts datnes saturs
+  
+  
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+for (var i = 0; i < files.length; i++) {
+
+
+for (var j = 0; j < files[i].length; j++) {
+
+ctx.fillRect(files[i][j],files[i][j], 2,2 );
+ctx.stroke();
+}
+}
     reader.onerror = (e) => alert(e.target.error.name);
     reader.readAsText(file); 
     
 });
-var ground = []; // Initialize array
-for (var i = 0 ; i < 15; i++) {
-    ground[i] = []; // Initialize inner array
-    for (var j = 0; j < 9; j++) { // i++ needs to be j++
-        ground[i][j] = (Math.random() * 5 | 0) + 6;
-    }
-}
